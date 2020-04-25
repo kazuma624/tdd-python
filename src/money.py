793f56d399995089300ca86800525ba99456ecba
Kazuma Hoda <saiteigundan@gmail.com>
2020-04-26 00:50:53 +0900

    第3章 三角測量

--- src/money.py
+++ src/money.py
@@ -4,3 +4,8 @@ class Dollar:
 
     def times(self, multiplier):
         return Dollar(self.amount * multiplier)
+
+    def equals(self, obj):
+        dollar = obj
+        # amount 属性の値同士が等しいかどうかを返す
+        return self.amount == dollar.amount
--- tests/test_money.py
+++ tests/test_money.py
@@ -4,12 +4,20 @@ import unittest
 
 class TestMoney(unittest.TestCase):
     def test_multiplication(self):
-        # Dollar クラスの宣言
+        # Dollar クラスの変数を宣言
         five = money.Dollar(5)
         product = five.times(2)
         self.assertEqual(10, product.amount)
         product = five.times(3)
         self.assertEqual(15, product.amount)
+
+    def test_equality(self):
+        self.assertTrue(
+            money.Dollar(5).equals(money.Dollar(5))
+        )
+        self.assertFalse(
+            money.Dollar(5).equals(money.Dollar(6))
+        )
 
 
 if __name__ == '__main__':
